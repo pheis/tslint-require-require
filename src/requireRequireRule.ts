c6dfdd9ce8668c80ae2af41fef36a22c6ca09b1d
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2019-04-03 17:28:16 +0300

    Implemen require require rule

--- src/requireRequireRule.ts
+++ src/requireRequireRule.ts
@@ -2,6 +2,9 @@ import * as Lint from "tslint";
 import * as ts from "typescript";
 import { findImports, ImportKind } from "tsutils";
 
+import * as fs from "fs";
+import * as path from "path";
+
 export class Rule extends Lint.Rules.AbstractRule {
   public static metadata: Lint.IRuleMetadata = {
     ruleName: "require-require",
@@ -15,7 +18,7 @@ export class Rule extends Lint.Rules.AbstractRule {
     typescriptOnly: false
   };
 
-  public static FAILURE_STRING = "Require require";
+  public static FAILURE_STRING = "File not found";
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
     return this.applyWithFunction(sourceFile, walk);
@@ -24,11 +27,16 @@ export class Rule extends Lint.Rules.AbstractRule {
 
 function walk(ctx: Lint.WalkContext) {
   for (const name of findImports(ctx.sourceFile, ImportKind.Require)) {
-    const { text } = name;
-    ctx.addFailure(
-      name.getStart(ctx.sourceFile) + 1,
-      name.end - 1,
-      Rule.FAILURE_STRING
-    );
+    const { text: base } = name;
+    const dir = path.dirname(ctx.sourceFile.fileName);
+    const filePath = path.join(dir, base);
+
+    if (!fs.existsSync(filePath)) {
+      ctx.addFailure(
+        name.getStart(ctx.sourceFile) + 1,
+        name.end - 1,
+        Rule.FAILURE_STRING
+      );
+    }
   }
 }
